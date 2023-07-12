0ee3a2d7cc977e48baba8d606ab1af5e84e18e65
vikrammanikantan <vik@arizona.edu>
2023-07-12 13:20:20 -0500

    added validation for redshift/distance

--- get_galaxies.py
+++ get_galaxies.py
@@ -18,11 +18,6 @@ def get_galaxies(ra='00h08m05.63s', dec='+14d50m23.3s', distance=0):
         returns table of galaxies and their 3D coordinates in the specified region
     """
 
-    # 1. get coordinates into SkyCoord
-    # 2. get list of database tables
-    # 3. loop through each database and find neighbors within distance
-    # 4. return astropy table with 
-
     from astropy.table import QTable
     import astropy.units as u
     import astropy.cosmology.units as cu
@@ -34,55 +29,73 @@ def get_galaxies(ra='00h08m05.63s', dec='+14d50m23.3s', distance=0):
     from astropy.coordinates import SkyCoord
     from astropy.table import Table, vstack
 
+    # rename the Heasarc database function for convenience
     heasarc = Heasarc()
 
-    ###
-    # Get list of tables to query from the database
-    ###
-
+    #####
+    # get list of mission databases available in heasarc catalog
     mission_list = heasarc.query_mission_list()
+    # from the different missions pull the 'Table' column where the value is 'Galaxy Catalog'
     tables = (mission_list[mission_list['Mission']=='GALAXY CATALOG'])['Table']
+    #####
 
-    ###
-    # format position
-    ###
+    #####
+    # format position into one string
+    pos = SkyCoord(str(ra)+' '+str(dec), frame='icrs')
+    #####
 
-    ra = '00h08m05.63s'
-    dec = '+14d50m23.3s'
-    pos = SkyCoord(ra+' '+dec, frame='icrs')
-
+    #####
+    # loop through tables, and initialize the galaxy table (gal_list) with the first valid galaxy
+    ra=None
     a=0
     while ra==None:
         try:
+            # get list of targets from specified table within given radius of our GW event
             targets = heasarc.query_region(pos, mission=tables[a], radius='5 degree')
-            
+    
+            # save the position and name of this target
             ra        = float(targets[a]['RA']) * u.deg
             dec       = float(targets[a]['DEC']) * u.deg
             #distance  = float(targets[a]['REDSHIFT']) * cu.redshift
             name      = str(targets[a]['NAME'])
             
+            # initialize the table with columns and one row of data
             gal_list = QTable([[name],[ra],[dec],[distance]], names=('Name', 'RA', 'DEC', 'Distance'), meta={'name': 'Galaxies in Region'})
-        except: None
+        except: 
+            ra=None
         a+=1
-    ra        = float(targets[0]['RA']) * u.deg
-    dec       = float(targets[0]['DEC']) * u.deg
-    #distance  = float(targets[0]['REDSHIFT']) * cu.redshift
-    name      = str(targets[0]['NAME'])
+    #####
 
-    gal_list = QTable([[name],[ra],[dec],[distance]], names=('Name', 'RA', 'DEC', 'Distance'), meta={'name': 'Galaxies in Region'})
-
-
+    #####
+    # for every table in the catalog, loop through and find targets near to the GW event
     for j in range(a, len(tables)):
+
+        # try to retrieve the table, if not possible, fast forward the loop to next iteration of j
         try:
             targets = heasarc.query_region(pos, mission=tables[j], radius='5 degree')
         except:
             break
+
+        # if table is available, then retrieve information of all targets and append to gal_list
         for i in range(0, len(targets)):
+            name      = str(targets[i]['NAME'])
             ra        = float(targets[i]['RA']) * u.deg
             dec       = float(targets[i]['DEC']) * u.deg
-            #distance  = float(targets[i]['REDSHIFT']) * cu.redshift
-            name      = str(targets[i]['NAME'])
-
+            
+            if "REDSHIFT" in targets[i].keys():
+                distance  = float(targets[i]['REDSHIFT']) * cu.redshift
+            elif "Distance" in targets[i].keys():
+                distance  = float(targets[i]['Distance']) * cu.redshift
+            elif "z" in targets[i].keys():
+                distance  = float(targets[i]['z']) * cu.redshift
+            else:
+                distance = -1 * cu.redshift
+            
+            # creating table with current target
             current_gal = QTable([[name],[ra],[dec],[distance]], names=('Name', 'RA', 'DEC', 'Distance'))
+            # merging current target with total target list (gal_list)
             gal_list    = vstack([gal_list, current_gal], join_type='inner')
+    #####
+    
+    # returning the full set of targets
     return gal_list
