52dfe2da451669c2cc7656df2bbfca6f173ef6b6
Vikram Manikantan <52188597+vikrammanikantan@users.noreply.github.com>
2023-07-13 15:35:41 -0500

    Update setup.py
    taken from orbitize!

--- setup.py
+++ setup.py
@@ -1,1 +1,44 @@
 ### setup.py for nomnom
+
+from setuptools import setup, find_packages, Extension
+import numpy, sys
+import re
+
+# auto-updating version code stolen from RadVel (via orbitize!)
+def get_property(prop, project):
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + "/__init__.py").read(),
+    )
+    return result.group(1)
+
+
+def get_requires():
+    reqs = []
+    for line in open("requirements.txt", "r").readlines():
+        reqs.append(line)
+    return reqs
+
+
+setup(
+    name="nomnom",
+    version=get_property("__version__", "nomnom"),
+    description="nomnom turns GW events into 3D visuals",
+    url="https://github.com/vikrammanikantan/nomnom",
+    author="Jessica Klusmeyer, Bill Smith, Vikram Manikantan",
+    author_email="nomnom@gmail.com",
+    license="BSD",
+    packages=find_packages(),
+    package_data={"": ["kernels/*.cu"]},
+    ext_modules=None,
+    include_dirs=[numpy.get_include()],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="Gravitational Wave Galaxy",
+    install_requires=get_requires(),
