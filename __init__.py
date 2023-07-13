8976edfe688afd0ec1758887f7478a1a18b7b414
Vikram Manikantan <52188597+vikrammanikantan@users.noreply.github.com>
2023-07-13 15:32:23 -0500

    Update __init__.py
    initial init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
 ### init.py for nomnom
+
+### adapted from orbitize! 
+### https://github.com/sblunt/orbitize
+
+import os
+
+__version__ = "0.0.1"
+
+# set Python env variable to keep track of example data dir
+nomnom_dir = os.path.dirname(__file__)
+
+#DATADIR = os.path.join(nomnom_dir, "example_data/")
